36ec704e6c3cd225b29faea3baf343d7dde3d395
Jordan Klassen <jordan@klassen.me.uk>
2016-11-01 16:38:00 -0700

    Don't fail on todo failures

---
+++ test/todo.js
@@ -0,0 +1,11 @@
+var t = require('tap');
+
+t.test('1 === 1', {todo: true}, function(assert) {
+  assert.plan(1);
+  assert.equal(1, 1);
+});
+
+t.test('2 === 2', {todo: true}, function(assert) {
+  assert.plan(1);
+  assert.equal(2, 2);
+});
